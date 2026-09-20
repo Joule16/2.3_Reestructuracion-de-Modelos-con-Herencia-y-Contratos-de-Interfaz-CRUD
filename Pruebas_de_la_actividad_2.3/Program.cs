d24a2e495a2c3c58cc79fa6a0c6e6fbeed99c0b9
Joule16 <joule.suniga2235@alumnos.udg.mx>
2026-09-20 03:20:44 -0600

    Add files via upload

---
+++ Pruebas_de_la_actividad_2.3/Program.cs
@@ -0,0 +1,22 @@
+//Suñiga Maciel Joule Alexander
+//Villa Olivares Ariel
+//Nuñes Martinez Marco Antonio
+//Equipo 2
+//=============================
+
+using SistemaBiblioteca1.Models;
+using System.Text;
+
+namespace SistemaBiblioteca1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            PruebasModelos.Mostrar();
+            Application.Run(new FrmPrincipal());
+        }
+    }
+}

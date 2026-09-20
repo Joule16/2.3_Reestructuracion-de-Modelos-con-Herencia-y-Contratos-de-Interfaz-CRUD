69dbe4630da34d5343052be9055b82124e5d5788
Joule16 <joule.suniga2235@alumnos.udg.mx>
2026-09-20 16:26:43 -0600

    Update print statement from 'Hello' to 'Goodbye'

--- FrmPrincipal.Designer.cs
+++ FrmPrincipal.Designer.cs
@@ -7,15 +7,10 @@ namespace SistemaBiblioteca1
 {
     partial class FrmPrincipal
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
+     
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+     
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -27,10 +22,7 @@ namespace SistemaBiblioteca1
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+
         private void InitializeComponent()
         {
             flowLayoutPanel1 = new FlowLayoutPanel();
